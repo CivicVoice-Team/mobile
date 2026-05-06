7e756d17be26969c31e9f6b17200b935aad97bee
unknown <mwh7061@rit.edu>
2026-05-06 14:27:10 -0400

    recycling banner

--- monroe-recycling-app/app/(tabs)/index.tsx
+++ monroe-recycling-app/app/(tabs)/index.tsx
@@ -13,7 +13,7 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/recyclingbanner.png')}
           style={styles.reactLogo}
         />
       }>
@@ -90,9 +90,11 @@ const styles = StyleSheet.create({
   },
   reactLogo: {
     height: 178,
-    width: 290,
+    width: '100%',
     bottom: 0,
     left: 0,
+    right: 0,
     position: 'absolute',
+    alignSelf: 'center',
   },
 });
