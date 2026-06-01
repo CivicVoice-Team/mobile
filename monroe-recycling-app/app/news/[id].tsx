feab7705235657b3038a161f877d1d1e92a38639
unknown <mwh7061@rit.edu>
2026-06-01 13:09:44 -0400

    back buttons added to news pages

--- monroe-recycling-app/app/news/[id].tsx
+++ monroe-recycling-app/app/news/[id].tsx
@@ -1,14 +1,20 @@
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { ScrollView, StyleSheet, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { ThemedText } from "@/components/themed-text";
 
 export default function NewsDetail() {
     const { title, description, date } = useLocalSearchParams();
+    const router = useRouter();
 
     const formattedDate = date ? new Date(date as string).toLocaleDateString("en-US") : "";
 
     return (
         <ScrollView contentContainerStyle={styles.container}>
+            <TouchableOpacity style={styles.backButton} onPress={() => router.push("/")}>
+                <Ionicons name="arrow-back" size={24} color="#456781" />
+            </TouchableOpacity>
+
             <ThemedText type="title">{title}</ThemedText>
 
             <ThemedText style={styles.date}>
@@ -25,6 +31,10 @@ export default function NewsDetail() {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
+  },
+
+  backButton: {
+    marginBottom: 16
   },
 
   date: {
